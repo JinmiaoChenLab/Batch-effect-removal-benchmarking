bb9fda39285fbbcfecf30788646e3495399fa757
KokSiong-Ang <koksiong.ang01@gmail.com>
2020-03-16 10:52:38 +0800

    Update to repository structure

---
+++ Script/Harmony/run_harmony_07.R
@@ -0,0 +1,93 @@
+
+# Author : Kok Siong Ang 
+# Date : 03/09/2019
+# Proj : Harmony pipeline
+
+########################
+#load packages
+
+library(harmony)
+library(cowplot)
+library(Seurat)  # Seurat 2 version
+library(magrittr)
+
+rm(list=ls())
+
+########################
+#settings
+
+filter_genes = T
+filter_cells = T
+normData = T
+Datascaling = T
+regressUMI = F
+min_cells = 10
+min_genes = 300
+norm_method = "LogNormalize"
+scale_factor = 10000
+b_x_low_cutoff = 0.0125
+b_x_high_cutoff = 3
+b_y_cutoff = 0.5
+numVG = 300
+npcs = 20
+visualize = T
+outfile_prefix = "Dataset7"
+save_obj = F
+
+src_dir = "./"
+working_dir = "../../Output/"
+read_dir = "../../Data/dataset7/"
+
+b1_exprs_filename = "b1_exprs.txt"
+b2_exprs_filename = "b2_exprs.txt"
+b1_celltype_filename = "b1_celltype.txt"
+b2_celltype_filename = "b2_celltype.txt"
+
+batch_label = "batchlb"
+celltype_label = "CellType"
+
+########################
+# read data 
+
+b1_exprs <- read.table(file = paste0(read_dir,b1_exprs_filename),sep="\t",header=T,row.names=1,check.names = F)
+b2_exprs <- read.table(file = paste0(read_dir,b2_exprs_filename),sep="\t",header=T,row.names=1,check.names = F)
+b1_celltype <- read.table(file = paste0(read_dir,b1_celltype_filename),sep="\t",header=T,row.names=1,check.names = F)
+b2_celltype <- read.table(file = paste0(read_dir,b2_celltype_filename),sep="\t",header=T,row.names=1,check.names = F)
+
+b1_celltype$cell <- rownames(b1_celltype)
+b1_celltype <- b1_celltype[colnames(b1_exprs),]
+b2_celltype$cell <- rownames(b2_celltype)
+b2_celltype <- b2_celltype[colnames(b2_exprs),]
+b1_metadata <- as.data.frame(b1_celltype)
+b2_metadata <- as.data.frame(b2_celltype)
+b1_metadata$batch <- 1
+b2_metadata$batch <- 2
+b1_metadata$batchlb <- 'Batch_1'
+b2_metadata$batchlb <- 'Batch_2'
+
+expr_mat = cbind(b1_exprs,b2_exprs)
+metadata = rbind(b1_metadata, b2_metadata)
+
+expr_mat <- expr_mat[, rownames(metadata)]
+
+########################
+# run pipeline
+
+source(paste0(src_dir,'call_harmony.R'))
+#setwd(working_dir)
+
+b_seurat = harmony_preprocess(
+                expr_mat, metadata, 
+                filter_genes = filter_genes, filter_cells = filter_cells,
+                normData = normData, Datascaling = Datascaling, regressUMI = regressUMI, 
+                min_cells = min_cells, min_genes = min_genes, 
+                norm_method = norm_method, scale_factor = scale_factor, 
+                b_x_low_cutoff = b_x_low_cutoff, b_x_high_cutoff = b_x_high_cutoff, b_y_cutoff = b_y_cutoff, 
+                numVG = numVG, npcs = npcs, 
+                batch_label = batch_label, celltype_label = celltype_label)
+
+b_seurat = call_harmony(b_seurat, batch_label, celltype_label, npcs, plotout_dir = working_dir, saveout_dir = working_dir, outfilename_prefix = outfile_prefix, visualize = visualize, save_obj = save_obj)
+
+
+
+
