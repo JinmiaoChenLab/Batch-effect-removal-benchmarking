229e17b537d16d9f3f87c6f6194651465023e80d
Kok Siong Ang <42174106+KokSiong-Ang@users.noreply.github.com>
2020-03-17 09:47:38 +0800

    Added fastMNN scripts

---
+++ Script/fastMNN/run_fastMNN_06.R
@@ -0,0 +1,103 @@
+
+# Author : Kok Siong Ang 
+# Date : 18/09/2019
+# Proj : Run fast MNN pipeline
+
+########################
+#load packages
+
+library(Seurat)  # Seurat 2 version
+library(scran)
+library(Rtsne)
+library(scales)
+
+rm(list=ls())
+
+########################
+#settings
+
+filter_genes = T
+filter_cells = T
+normData = T
+Datascaling = T
+regressUMI = F
+min_cells = 10
+min_genes = 300
+norm_method = "LogNormalize"
+scale_factor = 10000
+b_x_low_cutoff = 0.0125
+b_x_high_cutoff = 3
+b_y_cutoff = 0.5
+numVG = 300
+npcs = 20
+visualize = T
+outfile_prefix = "Dataset6"
+save_obj = F
+
+src_dir = "./"
+working_dir = "../../Output/"
+read_dir = "../../Data/dataset6/"
+
+b1_exprs_filename = "b1_exprs.txt"
+b2_exprs_filename = "b2_exprs.txt"
+b3_exprs_filename = "b3_exprs.txt"
+b1_celltype_filename = "b1_celltype.txt"
+b2_celltype_filename = "b2_celltype.txt"
+b3_celltype_filename = "b3_celltype.txt"
+
+batch_label = "batchlb"
+celltype_label = "CellType"
+
+########################
+# read data from flat text
+
+b1_exprs <- read.table(file = paste0(read_dir,b1_exprs_filename),sep="\t",header=T,row.names=1,check.names = F)
+b2_exprs <- read.table(file = paste0(read_dir,b2_exprs_filename),sep="\t",header=T,row.names=1,check.names = F)
+b3_exprs <- read.table(file = paste0(read_dir,b3_exprs_filename),sep="\t",header=T,row.names=1,check.names = F)
+b1_celltype <- read.table(file = paste0(read_dir,b1_celltype_filename),sep="\t",header=T,row.names=1,check.names = F)
+b2_celltype <- read.table(file = paste0(read_dir,b2_celltype_filename),sep="\t",header=T,row.names=1,check.names = F)
+b3_celltype <- read.table(file = paste0(read_dir,b3_celltype_filename),sep="\t",header=T,row.names=1,check.names = F)
+
+b1_celltype$cell <- rownames(b1_celltype)
+b1_celltype <- b1_celltype[colnames(b1_exprs),]
+b2_celltype$cell <- rownames(b2_celltype)
+b2_celltype <- b2_celltype[colnames(b2_exprs),]
+b3_celltype$cell <- rownames(b3_celltype)
+b3_celltype <- b3_celltype[colnames(b3_exprs),]
+b1_metadata <- as.data.frame(b1_celltype)
+b2_metadata <- as.data.frame(b2_celltype)
+b3_metadata <- as.data.frame(b3_celltype)
+b1_metadata$batch <- 1
+b2_metadata$batch <- 2
+b3_metadata$batch <- 3
+b1_metadata$batchlb <- 'Batch_1'
+b2_metadata$batchlb <- 'Batch_2'
+b3_metadata$batchlb <- 'Batch_3'
+
+expr_mat = cbind(b1_exprs, b2_exprs, b3_exprs)
+metadata = rbind(b1_metadata, b2_metadata, b3_metadata)
+
+expr_mat <- expr_mat[, rownames(metadata)]
+
+########################
+# run pipeline
+
+source(paste0(src_dir,'call_fastMNN_06.R'))
+
+batch_data = mnn_preprocess(
+                expr_mat, metadata, 
+                filter_genes = filter_genes, filter_cells = filter_cells,
+                normData = normData, Datascaling = Datascaling, regressUMI = regressUMI, 
+                min_cells = min_cells, min_genes = min_genes, 
+                norm_method = norm_method, scale_factor = scale_factor, 
+                b_x_low_cutoff = b_x_low_cutoff, b_x_high_cutoff = b_x_high_cutoff, b_y_cutoff = b_y_cutoff, 
+                numVG = numVG, npcs = npcs, 
+                batch_label = batch_label, celltype_label = celltype_label)
+data_list <- batch_data[[1]]
+meta_list <- batch_data[[2]]
+
+call_mnn(data_list, meta_list, batch_label, celltype_label, npcs, plotout_dir = working_dir, saveout_dir = working_dir, outfilename_prefix = outfile_prefix, visualize = visualize, save_obj = save_obj)
+
+
+
+
