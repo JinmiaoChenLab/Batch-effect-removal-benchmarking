e18330eaf49ac172745961adb7eea59e391fef41
Jinmiao Chen <chen_jinmiao@immunol.a-star.edu.sg>
2020-04-06 09:49:56 +0800

    Delete EPIC_ASIA.R

--- Script/EPIC_ASIA.R
+++
@@ -1,8 +0,0 @@
-rm(list=ls())
-
-library(Seurat)
-library(dplyr)
-
-expression_matrix <- Read10X(data.dir = "C:/Users/jinmiao/Desktop/temp")
-write.table(expression_matrix,"umi.txt",sep="\t")
-seurat_object = CreateSeuratObject(raw.data = expression_matrix)
