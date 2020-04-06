ec4941944de0f98177f1d4ae52ad8bb0bd5e5c8e
Nicole Y. S. Lee <54925231+lys-nicole@users.noreply.github.com>
2020-04-06 14:31:04 +0800

    ARI run scripts are added

---
+++ Script/eval_util_ARI/run_ARISampled_dataset2.R
@@ -0,0 +1,112 @@
+# Author : Nicole Lee
+# Date : 29/08/2019
+# Purpose: Primary script to run ARI pipeline
+
+# clear workspace
+rm(list=ls())
+
+# source relevant functions
+source("./ARI_utils/run_ARISampled.R")
+source("./ARI_utils/ari_calcul_sampled.R")
+source("./ARI_utils/conclude_ARISampled.R")
+
+##############################
+############ Set arguments
+
+eval_metric <- 'ARISampled'
+
+# read in files from this_dir
+this_dir<-"./generate_PCA_tSNE_UMAP/dataset2/"
+
+# send output to out_dir
+out_dir<-"./ARI_dataset2/"
+
+# create relevant folder within the out_dir
+dir.create(paste0(out_dir, eval_metric, "_CT"),showWarnings = FALSE)
+#dir.create(paste0(out_dir, eval_metric, "_OP"),showWarnings = FALSE)
+
+##############################
+############ Run 'run_ARISampled' function on relevant files
+
+methods_use <- 'ComBat'
+fn <- 'dataset2_combat_pca.csv'
+Rcombat<-run_ARISampled(fn,this_dir,out_dir,eval_metric,methods_use)
+
+methods_use <- 'MNN_Correct'
+fn <- 'dataset2_classicMNN_pca.csv'
+Rmnncorrect<-run_ARISampled(fn,this_dir,out_dir,eval_metric,methods_use)
+
+methods_use <- 'fastMNN'
+fn <- 'dataset2_fastMNN_pca.csv'
+Rfastmnn<-run_ARISampled(fn,this_dir,out_dir,eval_metric,methods_use)
+
+methods_use <- 'Harmony'
+fn <- 'dataset2_harmony_pca.csv'
+Rharmony<-run_ARISampled(fn,this_dir,out_dir,eval_metric,methods_use)
+
+methods_use <- 'LIGER'
+fn <- 'dataset2_liger_pca.csv'
+Rliger<-run_ARISampled(fn,this_dir,out_dir,eval_metric,methods_use)
+
+methods_use <- 'limma'
+fn <- 'dataset2_limma_pca.csv'
+Rlimma<-run_ARISampled(fn,this_dir,out_dir,eval_metric,methods_use)
+
+methods_use <- 'Raw'
+fn <- 'dataset2_raw_pca.csv'
+Rraw<-run_ARISampled(fn,this_dir,out_dir,eval_metric,methods_use)
+
+methods_use <- 'MMD-ResNet'
+fn <- 'dataset2_resnet_pca.csv'
+Rresnet<-run_ARISampled(fn,this_dir,out_dir,eval_metric,methods_use)
+
+methods_use <- 'Scanorama'
+fn <- 'dataset2_scanorama_pca.csv'
+Rscanorama<-run_ARISampled(fn,this_dir,out_dir,eval_metric,methods_use)
+
+methods_use <- 'scGen'
+fn <- 'dataset2_scgen_pca.csv'
+Rscgen<-run_ARISampled(fn,this_dir,out_dir,eval_metric,methods_use)
+
+methods_use <- 'scMerge'
+fn <- 'dataset2_scmerge_pca.csv'
+Rscmerge<-run_ARISampled(fn,this_dir,out_dir,eval_metric,methods_use)
+
+methods_use <- 'ZINB-WaVE'
+fn <- 'dataset2_zinbwave_pca.csv'
+Rzinbwave<-run_ARISampled(fn,this_dir,out_dir,eval_metric,methods_use)
+
+methods_use <- 'Seurat_2'
+fn <- 'dataset2_seurat2_pca.csv'
+Rseurat2<-run_ARISampled(fn,this_dir,out_dir,eval_metric,methods_use)
+
+methods_use <- 'Seurat_3'
+fn <- 'dataset2_seurat3_pca.csv'
+Rseurat3<-run_ARISampled(fn,this_dir,out_dir,eval_metric,methods_use)
+####################################################################
+
+##############################
+############ Extracting all data from all methods in dataset 
+
+# Reads files from dir_this 
+dir_this<-paste0(out_dir, eval_metric, "_CT")
+#dir_this<-paste0(out_dir, eval_metric, "_OP")
+
+# Perform the following function to produce final CSV file
+wholedf<-conclude_ARISampled(dir_this, "Dataset_2") 
+
+save.image(paste0("Dataset2", "_complete.RData"))
+
+####################################################################
+
+##############################
+############ Consolidate all raw data into one file
+
+setwd(dir_this)
+rm(list=ls())
+
+source("./ARI_utils/ARI_files_consolidate.R")
+ 
+ari_consolidate()
+
+### END
