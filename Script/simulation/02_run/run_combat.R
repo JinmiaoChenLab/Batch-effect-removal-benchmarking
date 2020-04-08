bebbf322fb9e951125b81986660b4eb693b24388
Marion Chevrier <44140653+marionchvr@users.noreply.github.com>
2020-04-08 19:18:45 +0800

    Update run_combat.R

--- Script/simulation/02_run/run_combat.R
+++ Script/simulation/02_run/run_combat.R
@@ -4,14 +4,11 @@ detach("package:ezTools", unload=TRUE)
 library("Seurat", lib.loc="~/R/x86_64-pc-linux-gnu-library/3.6")
 library(sva)
 
-this.dir <- '/home/marion/Marion/Project/Hoa_batch_normalization/simulation_dataset_V3/'
-setwd(this.dir)
-
-lsdir <- list.dirs('data', recursive=FALSE) 
+lsdir <- list.dirs('Data/dataset3/', recursive=FALSE) 
 
 sapply(lsdir,function(x){
 
-  x2 <- gsub('data/','',x)
+  x2 <- gsub('Data/dataset3/','',x)
   dir.create(paste0('demo_Combat/',x2), showWarnings = FALSE)
   
   selection <- c('HVG','all')
